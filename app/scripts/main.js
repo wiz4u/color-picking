af5ab22d81604c4d181eabec2edea82c4bd0402f
Takeshi YAEDA <t.yaeda@gmail.com>
2014-05-28 01:15:13 +0900

    Implement rear camera testing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,70 @@
-console.log('\'Allo \'Allo!');
+(function () {
+    'use strict';
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia;
+
+    var changeCameraElement = document.getElementsByClassName('change-camera');
+    var videoElement = document.getElementById('video');
+    var videoSourceInfo = [];
+    var currentVideoSourceInfoId = null;
+    var stream = null;
+
+    var gotSources = function (sourceInfos) {
+        for (var i = 0; i !== sourceInfos.length; ++i) {
+            var sourceInfo = sourceInfos[i];
+            if (sourceInfo.kind === 'video') {
+                videoSourceInfo.push(sourceInfo.id);
+            }
+        }
+        if (videoSourceInfo.length) {
+            currentVideoSourceInfoId = 0;
+        }
+
+        start();
+    };
+
+    var changeCamera = function () {
+        var numSource = videoSourceInfo.length;
+        if (numSource) {
+            currentVideoSourceInfoId = (currentVideoSourceInfoId + 1) % numSource;
+        }
+
+        start();
+    };
+
+    var successCallback = function (aStream) {
+        stream = aStream;
+        videoElement.src = window.URL.createObjectURL(stream);
+        videoElement.play();
+    };
+
+    var errorCallback = function (error) {
+        console.log('navigator.getUserMedia error: ', error);
+    };
+
+    var start = function () {
+        if (!!stream) {
+            videoElement.src = null;
+            stream.stop();
+        }
+
+        var constraints = {
+            video: {
+                optional: [{sourceId: currentVideoSourceInfoId}]
+            }
+        };
+
+        navigator.getUserMedia(constraints, successCallback, errorCallback);
+    };
+
+    if (typeof window.MediaStreamTrack === 'undefined'){
+        window.alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
+    } else {
+        window.MediaStreamTrack.getSources(gotSources);
+    }
+
+    changeCameraElement[0].onchange = changeCamera;
+
+})();
