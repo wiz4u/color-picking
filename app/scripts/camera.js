f59e135e60fdf9edaf77df606bc02635826f7dd7
Takeshi YAEDA <t.yaeda@gmail.com>
2014-06-07 15:02:28 +0900

    :lipstick: Add camera class

---
+++ app/scripts/camera.js
@@ -0,0 +1,117 @@
+(function (CP) {
+    'use strict';
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia ||
+                             navigator.msGetUserMedida;
+
+    var Camera = function (element, callback) {
+        this.element = element || document.createElement('video');
+        this.stream = null;
+
+        this.videoSourceInfo = [];
+        this.audioSourceInfo = [];
+        this.currentVideoSourceInfoIndex = 0;
+        this.currentAudioSourceInfoIndex = 0;
+
+        var self = this;
+        var _callback = callback || function () {};
+        var _gotSources = function (sourceInfos) {
+            for (var i = 0, l = sourceInfos.length; i < l; ++i) {
+                var sourceInfo = sourceInfos[i];
+                if (sourceInfo.kind === 'video') {
+                    self.videoSourceInfo.push(sourceInfo.id);
+                } else if (sourceInfo.kind === 'audio') {
+                    self.audioSourceInfo.push(sourceInfo.id);
+                }
+            }
+
+            if (self.videoSourceInfo.length) {
+                // In general, rear camera is last one.
+                self.currentVideoSourceInfoIndex = self.videoSourceInfo.length - 1;
+            }
+            if (self.audioSourceInfo.length) {
+                self.currentAudioSourceInfoIndex = 0;
+            }
+
+            _callback();
+        };
+
+        if (typeof window.MediaStreamTrack !== 'undefined' &&
+            typeof window.MediaStreamTrack.getSources !== 'undefined') {
+            window.MediaStreamTrack.getSources(_gotSources);
+        } else {
+            setTimeout(_callback, 0);
+        }
+    };
+
+    Camera.prototype.initialize = function (successCallback, errorCallback) {
+        // stop stream
+        if (!!this.stream) {
+            this.element.src = null;
+            this.stream.stop();
+        }
+
+        // build callback functions
+        var self = this;
+        var _successCallback = function (stream) {
+            self.stream = stream;
+            self.element.src = window.URL.createObjectURL(stream);
+            self.element.play();
+
+            if (successCallback) {
+                successCallback();
+            }
+        };
+
+        var _errorCallback = function (error) {
+            console.log('navigator.getUserMedia error:', error);
+
+            if (errorCallback) {
+                errorCallback();
+            }
+        };
+
+        // build constraints object
+        var constraints = null;
+        if (this.videoSourceInfo.length) {
+            var videoSourceId = this.videoSourceInfo[this.currentVideoSourceInfoIndex];
+            var audioSourceId = this.audioSourceInfo[this.currentAudioSourceInfoIndex];
+            constraints = {
+                video: {
+                    optional: [{sourceId: videoSourceId}]
+                },
+                audio: {
+                    optional: [{sourceId: audioSourceId}]
+                }
+            };
+        } else {
+            constraints = {
+                video: true,
+                audio: true
+            };
+        }
+
+        // get camera stream
+        navigator.getUserMedia(constraints, _successCallback, _errorCallback);
+    };
+
+    Camera.prototype.changeCamera = function () {
+        var numCamera = this.videoSourceInfo.length;
+        if (numCamera && numCamera > 1) {
+            var index = this.currentVideoSourceInfoIndex;
+            this.currentVideoSourceInfoIndex = (index + 1) % numCamera;
+
+            this.initialize();
+        }
+    };
+
+    Camera.prototype.getElement = function () {
+        return this.element;
+    };
+
+    // Export
+    CP.Camera = Camera;
+
+})(window.CP = window.CP || {});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,97 +1,16 @@
 (function () {
     'use strict';
 
-    navigator.getUserMedia = navigator.getUserMedia ||
-                             navigator.webkitGetUserMedia ||
-                             navigator.mozGetUserMedia ||
-                             navigator.msGetUserMedia;
-
-    var changeCameraElement = document.getElementsByClassName('change-camera');
+    // camera
     var videoElement = document.getElementById('video');
-    var videoSourceInfo = [];
-    var audioSourceInfo = [];
-    var currentVideoSourceInfoId = null;
-    var currentAudioSourceInfoId = null;
-    var stream = null;
+    var camera = new window.CP.Camera(videoElement, function () {
+        camera.initialize();
+    });
 
-    var canChangeCamera = false;
-
-    var gotSources = function (sourceInfos) {
-        for (var i = 0; i !== sourceInfos.length; ++i) {
-            var sourceInfo = sourceInfos[i];
-            if (sourceInfo.kind === 'video') {
-                videoSourceInfo.push(sourceInfo.id);
-            } else if (sourceInfo.kind === 'audio') {
-                audioSourceInfo.push(sourceInfo.id);
-            }
-        }
-
-        if (videoSourceInfo.length) {
-            // In general, rear camera is last one.
-            currentVideoSourceInfoId = videoSourceInfo.length - 1;
-        }
-        if (audioSourceInfo.length) {
-            currentAudioSourceInfoId = 0;
-        }
-
-        start();
+    // change camera button
+    var changeCameraElement = document.getElementsByClassName('change-camera');
+    changeCameraElement[0].onclick = function () {
+        camera.changeCamera();
     };
-
-    var changeCamera = function () {
-        var numSource = videoSourceInfo.length;
-        if (numSource) {
-            currentVideoSourceInfoId = (currentVideoSourceInfoId + 1) % numSource;
-        }
-
-        start();
-    };
-
-    var successCallback = function (aStream) {
-        stream = aStream;
-        videoElement.src = window.URL.createObjectURL(stream);
-        videoElement.play();
-    };
-
-    var errorCallback = function (error) {
-        console.log('navigator.getUserMedia error: ', error);
-    };
-
-    var start = function () {
-        if (!!stream) {
-            videoElement.src = null;
-            stream.stop();
-        }
-
-        var constraints = null;
-        if (canChangeCamera) {
-            var audioSourceId = audioSourceInfo[currentAudioSourceInfoId];
-            var videoSourceId = videoSourceInfo[currentVideoSourceInfoId];
-            constraints = {
-                audio: {
-                    optional: [{sourceId: audioSourceId}]
-                },
-                video: {
-                    optional: [{sourceId: videoSourceId}]
-                }
-            };
-        } else {
-            constraints = {
-                audio: true,
-                video: true
-            };
-        }
-
-        navigator.getUserMedia(constraints, successCallback, errorCallback);
-    };
-
-    if (typeof window.MediaStreamTrack === 'undefined' ||
-        typeof window.MediaStreamTrack.getSources === 'undefined'){
-        window.alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
-        start();
-    } else {
-        canChangeCamera = true;
-        window.MediaStreamTrack.getSources(gotSources);
-        changeCameraElement[0].onclick = changeCamera;
-    }
 
 })();
