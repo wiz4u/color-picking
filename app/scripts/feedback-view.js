cf220407b1f102fb89ee2854f80198b319418d6f
Takeshi YAEDA <t.yaeda@gmail.com>
2014-06-23 22:53:34 +0900

    :yum: Enable to send feedback to the server
    - Open modal for feedback
    - Create Parse object
    - Send object to the parse server

---
+++ app/scripts/feedback-view.js
@@ -0,0 +1,62 @@
+(function (CP) {
+    'use strict';
+
+    var FeedbackView = function () {};
+
+    var BROWSER_TYPES = [
+        'Android : Defautl Browser',
+        'Android : Chrome',
+        'Android : Chrome Beta',
+        'Android : Firefox',
+        'Android : Firefox Beta',
+        'Android : Opera',
+        'Android : Opera Mini',
+        'Android : Opera Beta',
+        'Android : Another one',
+        'iOS',
+        'Mac OS',
+        'Windows',
+        'Another Platform'
+    ];
+
+    FeedbackView.prototype.initialize = function () {
+        this._onSend = this.onSend.bind(this);
+        this.$element = $('#feedback_modal');
+        this.$btnSend = $('#feedback_modal .feedback_send')
+            .on('click', this._onSend);
+        return this;
+    };
+
+    FeedbackView.prototype.finalize = function () {
+        this.$btnSend.off('click', this._onSend);
+    };
+
+    FeedbackView.prototype.render = function () {
+        var $select = $('#feedback_browser_type');
+        for (var i = 0, l = BROWSER_TYPES.length; i < l; i++) {
+            var browserType = BROWSER_TYPES[i];
+            $('<option>')
+                .attr({value: browserType})
+                .text(browserType)
+                .appendTo($select);
+        }
+        return this;
+    };
+
+    FeedbackView.prototype.onSend = function () {
+        var browserType = $('#feedback_browser_type > :selected').val();
+        var comment = $('#feedback_free_text').val();
+
+        this.model = new CP.Feedback();
+        this.model.set('browser_type', browserType);
+        this.model.set('comment', comment);
+        this.model.save();
+
+        // close modal
+        this.$element.modal('hide');
+    };
+
+    // Export
+    CP.FeedbackView = FeedbackView;
+
+})(window.CP = window.CP || {});
---
+++ app/scripts/model/feedback.js
@@ -0,0 +1,8 @@
+/* global Parse */
+
+(function (CP) {
+    'use strict';
+
+    CP.Feedback = Parse.Object.extend('Feedback');
+
+})(window.CP = window.CP || {});
