fc9be196cd2f76dea258a4cec7751d12a0ff6318
Takeshi YAEDA <t.yaeda@gmail.com>
2014-06-30 22:55:22 +0900

    :memo: Fix a typo in comment

--- app/scripts/time-attack-game.js
+++ app/scripts/time-attack-game.js
@@ -7,7 +7,7 @@
         this.scoreBoardView = scoreBoardView;
 
         this.GAME_MODE_NAME = 'Time Attack Mode';
-        this.TOTAL_TIME_MS = 1000 * 30; // 30 [min]
+        this.TOTAL_TIME_MS = 1000 * 30; // 30 [sec]
         this.SCORE_THRESH = 80;
 
         this.endTime = null;
